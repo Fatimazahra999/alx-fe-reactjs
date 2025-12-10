72357ba99bbe81ee8f698743fac84d9e2a77bbd6
Fatimazahra999 <fatimazahrabendriouich@gmail.com>
2025-12-10 16:29:07 +0100

    Refactor App component and add CSS import

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,9 +1,10 @@
- import React from "react";
-import TodoList from "./components/TodoList";
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <TodoList />
     </div>
   );
